ad2af4a9ab4e5615f1d0496e7dbeb10be2050613
Ian Cenry <iancenry@gmail.com>
2023-08-23 22:54:37 +0300

    feat: handle edit

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -9,7 +10,8 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-  const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
   const handleDone = (id: number) => {
     setTodos(
@@ -25,14 +27,29 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
 
   return (
     <form className="todos__single">
-      {todo.isDone ? (
+      {edit ? (
+        <input
+          type="text"
+          className="todos__single--text"
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
         <s className="todos__single--text">{todo.todo}</s>
       ) : (
         <span className="todos__single--text">{todo.todo}</span>
       )}
 
       <div style={{ display: 'flex' }}>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            // if edit mode off and not done then allow edit
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
         <span className="icon" onClick={() => handleDelete(todo.id)}>
