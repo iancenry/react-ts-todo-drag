f322cb462f162c2d98ff9a2d191e663d5b881b18
Ian Cenry <iancenry@gmail.com>
2023-08-24 22:02:12 +0300

    chore: provide DragDrop context

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,11 +22,18 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="app">
-      <span className="app__heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="app">
+        <span className="app__heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
