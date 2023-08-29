4d68fba8fe9e10cb463fc247d426c241a6149197
Ian Cenry <iancenry@gmail.com>
2023-08-29 21:07:12 +0300

    chore: add context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,23 @@
-import { useState, useMemo } from 'react';
 import './App.css';
 import InputField from './components/InputField';
-import { ITodo, IContextProps } from './@types/todo';
 import TodoList from './components/TodoList';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
-import { TodoContext } from './context/todoContext';
+import TodoProvider from './context/todoContext';
+import ThemeWrapper from './components/ThemeWrapper';
+import ThemeProvider from './context/themeContext';
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>('');
-  const [todos, setTodos] = useState<ITodo[]>([]);
-  const [completedTodos, setCompletedTodos] = useState<ITodo[]>([]);
-  const contextProviderValues: IContextProps = useMemo(
-    () => ({
-      todo,
-      todos,
-      setTodo,
-      setTodos,
-      completedTodos,
-      setCompletedTodos,
-    }),
-    [todo, todos, setTodo, setTodos, completedTodos, setCompletedTodos]
-  );
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos((prevtodos) => [
-        ...prevtodos,
-        { id: Date.now(), todo, isDone: false },
-      ]);
-      setTodo('');
-    }
-  };
-
-  const onDragEnd = (result: DropResult) => {
-    const { source, destination } = result;
-
-    if (!destination) return;
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    )
-      return;
-
-    let add;
-    const active = todos,
-      complete = completedTodos;
-
-    if (source.droppableId === 'TodosList') {
-      add = active[source.index];
-      active.splice(source.index, 1);
-    } else {
-      add = complete[source.index];
-      complete.splice(source.index, 1);
-    }
-
-    if (destination.droppableId === 'TodosList') {
-      active.splice(destination.index, 0, add);
-    } else {
-      complete.splice(destination.index, 0, add);
-    }
-
-    setCompletedTodos(complete);
-    setTodos(active);
-  };
-
   return (
-    <TodoContext.Provider value={contextProviderValues}>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <div className="app">
-          <span className="app__heading">Taskify</span>
-          <InputField handleAdd={handleAdd} />
-          <TodoList />
-        </div>
-      </DragDropContext>
-    </TodoContext.Provider>
+    <ThemeProvider>
+      <TodoProvider>
+        <ThemeWrapper>
+          <div className="app">
+            <span className="app__heading">Taskify</span>
+            <InputField />
+            <TodoList />
+          </div>
+        </ThemeWrapper>
+      </TodoProvider>
+    </ThemeProvider>
   );
 };
 
